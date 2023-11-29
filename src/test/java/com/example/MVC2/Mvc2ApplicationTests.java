d82fec21ad3bbb47ea8d1df0fdee26b7563a43ed
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-11-29 11:50:31 +0530

    Add files via upload

---
+++ src/test/java/com/example/MVC2/Mvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MVC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
